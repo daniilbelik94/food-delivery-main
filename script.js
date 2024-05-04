65706ace09a5c8601a6d36fc1a8391501bd09d3a
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-05-04 17:58:07 +0200

    320px
    with modal window and js is-open and close buttons

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.querySelector(".backdrop");
+
+    const openButtons = document.querySelectorAll(".button__section-one, .section__button");
+    const closeButton = document.querySelector(".modal-close__btn");
+
+
+    function openModal() {
+        modal.classList.add("is-open");
+        body.classList.add("modal-open"); 
+    }
+
+    function closeModal() {
+        modal.classList.remove("is-open");
+        body.classList.remove("modal-open"); 
+    }
+
+    openButtons.forEach(function (button) {
+        button.addEventListener("click", openModal);
+    });
+
+    if (closeButton) {
+        closeButton.addEventListener("click", closeModal);
+    }
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+});
 const burgerIcon = document.getElementById('burgerIcon');
 const burgerMenu = document.getElementById('burgerMenu');
 const burgerIconClose = document.getElementById('burgerIconClose');
