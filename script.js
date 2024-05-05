fb72c85f37a1a247222be6943945b2be519dd536
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-05-05 12:13:22 +0200

    sunday upd
    with swiper 768px

--- script.js
+++ script.js
@@ -54,31 +54,25 @@ document.addEventListener('click', function(event) {
         burgerMenu.classList.remove('hidden');
     }
 });
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 292;
+    if (offset > 584) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 292;
+    if (offset < 0) {
+        offset = 584;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
